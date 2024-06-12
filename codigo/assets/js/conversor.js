c41c5243e8c8e6de0c84b39519df9f9882c5d722
Arthur 'sevak' Costa Serra Negra <163688467+sevak19@users.noreply.github.com>
2024-06-12 17:28:03 -0300

    update

--- codigo/assets/js/conversor.js
+++ codigo/assets/js/conversor.js
@@ -1,61 +1,96 @@
-$(document).ready(function() {
-    $('#valorOrigem, #moedaOrigem, #moedaDestino').on('input change', function() {
-        converterMoeda();
-    });
-});
-
-function converterMoeda() {
-    
-    var moedaOrigem = $('#moedaOrigem').val();
+async function converterMoeda() {
     var moedaDestino = $('#moedaDestino').val();
-    var valorOrigem = parseFloat($('#valorOrigem').val());
+    var simbolo;
 
-    if (moedaOrigem == 'real') {
-        $('#moedaorigem').text("R$");
-    } else if (moedaOrigem == 'dolar') {
-        $('#moedaorigem').text("$");
-    } else if (moedaOrigem == 'euro') {
-        $('#moedaorigem').text("€");
+    if (moedaDestino === 'MOEDA DESTINO') {
+        $('.gastos').hide();
+        $('.lista-gastos h1').hide();
+        $('.categorias').hide();
+        return;
     }
-
 
     if (moedaDestino == 'real') {
-        $('#moedaresultado').text("R$");
+        simbolo = 'R$';
     } else if (moedaDestino == 'dolar') {
-        $('#moedaresultado').text("$");
+        simbolo = '$';
     } else if (moedaDestino == 'euro') {
-        $('#moedaresultado').text("€");
+        simbolo = '€';
     }
 
+    $('.gastos').show();
+    $('.lista-gastos h1').show();
+    $('.categorias').show();
 
-    
-    var taxaDolarParaReal = 5.3;
-    var taxaDolarParaEuro = 0.8;
-    var taxaEuroParaReal = 6.6;
-    var taxaEuroParaDolar = 1.25;
+    const response = await fetch('https://1a770c80-41ff-4f19-8cb5-699967f55bcb-00-1395yp0me7mv2.kirk.replit.dev/extrato');
+    const data = await response.json();
+
     var taxaRealParaDolar = 0.19;
     var taxaRealParaEuro = 0.15;
 
-    
-    var resultado;
+    var extratoHTML = '';
+    var labels = [];
+    var graphData = [];
 
-    if (moedaOrigem === 'dolar' && moedaDestino === 'real') {
-        resultado = valorOrigem * taxaDolarParaReal;
-    } else if (moedaOrigem === 'dolar' && moedaDestino === 'euro') {
-        resultado = valorOrigem * taxaDolarParaEuro;
-    } else if (moedaOrigem === 'euro' && moedaDestino === 'real') {
-        resultado = valorOrigem * taxaEuroParaReal;
-    } else if (moedaOrigem === 'euro' && moedaDestino === 'dolar') {
-        resultado = valorOrigem * taxaEuroParaDolar;
-    } else if (moedaOrigem === 'real' && moedaDestino === 'dolar') {
-        resultado = valorOrigem * taxaRealParaDolar;
-    } else if (moedaOrigem === 'real' && moedaDestino === 'euro') {
-        resultado = valorOrigem * taxaRealParaEuro;
-    } else {
-        resultado = valorOrigem;
-    }
+    data.forEach(item => {
+        var valorOrigem = parseFloat(item.valor);
+        var resultado = valorOrigem;
 
-    if (valorOrigem) {
-        $('#resultado').val(resultado.toFixed(2));
+        if (item.moeda === 'real' && moedaDestino === 'dolar') {
+            resultado = valorOrigem * taxaRealParaDolar;
+        } else if (item.moeda === 'real' && moedaDestino === 'euro') {
+            resultado = valorOrigem * taxaRealParaEuro;
+        }
+
+        if (!isNaN(resultado)) {
+            var tipo = item.tipo === "Gasto" ? "gasto" : "ganho";
+            extratoHTML += `<li class="${tipo}">${simbolo} ${resultado.toFixed(2)} (${item.descricao})</li>`;
+            labels.push(item.descricao);
+            graphData.push(parseFloat(resultado.toFixed(2))); // Corrige para ser um número em vez de string
+        }
+    });
+
+    $('.extrato').html(extratoHTML);
+
+    const ctx = document.getElementById('grafico');
+
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels,
+            datasets: [{
+                data: graphData,
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)',
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)',
+                ],
+                hoverOffset: 4
+            }]
+        },
+    });
+}
+
+$(document).ready(function() {
+    // Inicialmente esconde os elementos de gastos e ganhos
+    $('.gastos').hide();
+    $('.lista-gastos h1').hide();
+    $('.categorias').hide();
+
+    $('#moedaDestino').on('change', function() {
+        converterMoeda();
+    });
+
+    $('.btn-todos').on('click', function() {
+        filtrarCategoria('todos');
+    });
+});
+
+function filtrarCategoria(categoria) {
+    $('li').show();
+    if (categoria !== 'todos') {
+        $('li').not('.' + categoria).hide();
     }
 }
